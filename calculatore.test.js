f26621cd73a6d1c0294bdda5db7b67be762b06b3
david-lafontant <lafontant.david@gmail.com>
2022-02-03 08:36:18 -0600

    Add test

---
+++ calculatore.test.js
@@ -0,0 +1,54 @@
+
+
+const calculator = require('./calculator');
+
+
+describe('My calculator', () => {
+
+    test('minus 5 - 2 to equal 3', () => {
+        expect(calculator.minus(5, 2)).toBe(3);
+    });
+
+    test('minus 5 - 4 to equal 1', () => {
+        expect(calculator.minus(5, 4)).toBe(1);
+    });
+
+    test('minus 1000 - 2 to equal 998', () => {
+        expect(calculator.minus(1000, 2)).toBe(998);
+    });
+
+    test('Add 5 + 2 to equal 7', () => {
+        expect(calculator.add(5, 2)).toBe(7);
+    });
+
+    test('add 5 + 4 to equal 9', () => {
+        expect(calculator.add(5, 4)).toBe(9);
+    });
+
+    test('add 1000 + 2 to equal 1002', () => {
+        expect(calculator.add(1000, 2)).toBe(1002);
+    });
+
+    test('divide 5 + 2 to equal 3.5', () => {
+        expect(calculator.divide(5, 2)).toBe(3.5);
+    });
+
+    test('divide 8/4 to equal 2', () => {
+        expect(calculator.divide(8/4)).toBe(2);
+    });
+
+    test('divide 1000 / 2 to equal 1002', () => {
+        expect(calculator.divide(1000, 2)).toBe(500);
+    });
+
+
+
+
+
+
+
+
+
+
+});
+
